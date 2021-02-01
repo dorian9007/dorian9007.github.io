6af6290b95b59c149fceb0021278e0eabfcf8409
dorian9007 <44480951+dorian9007@users.noreply.github.com>
2021-02-01 18:51:54 +0100

    Create contactanimation.js

---
+++ matrix/contactanimation.js
@@ -0,0 +1,33 @@
+<script>
+(function () {
+   var CharacterPos = 0;
+   var MsgBuffer = "";
+   var TypeDelay = 75; 
+   var NxtMsgDelay = 1000;
+   var MsgIndex = 0;
+   var delay;
+   var MsgArray = ["contact me"];
+
+   function StartTyping() {
+      var id = document.getElementById("typing-text");
+      if (CharacterPos != MsgArray[MsgIndex].length) {
+         MsgBuffer  = MsgBuffer + MsgArray[MsgIndex].charAt(CharacterPos);
+         id.value = MsgBuffer+"_";
+         delay = TypeDelay;
+         id.scrollTop = id.scrollHeight; 
+      } else {
+         delay = NxtMsgDelay;
+         MsgBuffer   = "";
+         CharacterPos = -1;
+         if (MsgIndex!=MsgArray.length-1){
+           MsgIndex++;
+         }else {
+           MsgIndex = 0;
+         }
+       }
+       CharacterPos++;
+       setTimeout(StartTyping,delay);
+   }
+StartTyping();
+})();
+</script>
